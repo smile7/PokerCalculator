b808138a1b19d1c14b71f1a58794bb29f6f24475
smile7 <sisitymiteva@yahoo.com>
2015-07-29 13:23:39 +0300

    reads input and shows it

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
-var texas = require('texas');
+window.onload = function () {
+    var inputP1 = document.getElementById("p1"),
+        inputP2 = document.getElementById("p2"),
+        inputFlop = document.getElementById("flop"),
+        inputTurn = document.getElementById("turn"),
+        inputRiver = document.getElementById("river"),
+        outputP1 = document.getElementById("output-p1"),
+        outputP2 = document.getElementById("output-p2"),
+        btnGO = document.getElementById("go"),
+        formOut = document.getElementById("output");
 
-var hand = texas.deck();
+    function showInputP1() {
+        outputP1.innerHTML = inputP1.value;
+
+        if(inputP1.innerHTML === '') {
+
+        } else {
+            formOut.appendChild(inputP1);
+            input.value = '';
+        }
+    }
+
+    function showInputP2() {
+        outputP2.innerHTML = inputP2.value;
+
+        if(inputP2.innerHTML === '') {
+
+        } else {
+            formOut.appendChild(inputP2);
+            input.value = '';
+        }
+    }
+
+    btnGO.addEventListener("click", showInputP1);
+    btnGO.addEventListener("click", showInputP2);
+};
